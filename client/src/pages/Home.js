d437ccf103d95f30dfc70c08da6b929daaedade5
erik antony <erik.antony.mg@gmail.com>
2022-01-05 17:11:58 -0500

    main

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { useQuery } from '@apollo/react-hooks';
-import { Grid, Transition } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 import gql from 'graphql-tag'
 import { AuthContext } from '../context/auth';
 import PostCard from '../components/PostCard';
 import PostForm from '../components/PostForm';
-import { FETCH_POSTS_QUERY } from '../util/graphql';
+
 
 function Home() {
   const { user } = useContext(AuthContext);
